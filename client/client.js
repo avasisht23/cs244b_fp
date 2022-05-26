b843f5fc9192d2eaa33a4d8db988e3163429a57b
Your Name <you@example.com>
2022-05-25 18:15:39 -0700

    add

--- client/client.js
+++ client/client.js
@@ -19,7 +19,7 @@ const sleepCadence = 30000 // 30 seconds
 // Appends order to hotstuff ledger
 async function append(order, hashedOrder){
   for (const port of hotStuffPorts) {
-    axios.post(`http://localhost:${port}`, {order: hashedOrder})
+    await axios.post(`http://localhost:${port}`, {order: hashedOrder})
       .then(function (response) {
         console.log(`Successfully submitted ${order.side} order for asset $${order.asset} @ ${order.limitPrice} to HotStuff Node ${port}`);
         if(response.data.isLeader){
@@ -34,10 +34,10 @@ async function append(order, hashedOrder){
 
 // Get client id
 async function getNewClientId(){
-  axios.get(`http://localhost:${darkpoolPort}/getNewClientId`)
+  await axios.get(`http://localhost:${darkpoolPort}/getNewClientId`)
     .then(function (response) {
       console.log(`Successfully got new client id`);
-      return response.data.clientId.toString();
+      return response.data.clientId;
     })
     .catch(function (error) {
       console.log(`Failed to get new client id`);
@@ -47,7 +47,7 @@ async function getNewClientId(){
 // Gets index of order in hotstuff ledger
 async function getIndex(order, hashedOrder){
   for (const port of hotStuffPorts) {
-    axios.get(`http://localhost:${port}/index?order=${hashedOrder}`)
+    await axios.get(`http://localhost:${port}/index?order=${hashedOrder}`)
       .then(function (response) {
         console.log(`Successfully queried ${order.side} order for asset $${order.asset} @ ${order.limitPrice} from HotStuff Node ${port}`);
         if(response.data.isLeader){
@@ -71,7 +71,7 @@ async function main() {
       clientId: clientId
     }
 
-  let hashedOrder = createHash('sha256').update(`${asset}${limitPrice}${side}`).digest('hex') + "," + clientId
+  let hashedOrder = createHash('sha256').update(`${asset}${limitPrice}${side}`).digest('hex') + "," + clientId.toString()
 
   // 1. append(order) —> to Hotstuff via REST
   let ourIndex = await append(order, hashedOrder);
@@ -79,7 +79,7 @@ async function main() {
   var token;
 
   // 2. submit order to darkpool
-  axios.post(`http://localhost:${darkpoolPort}/sendOrder`, JSON.stringify(order))
+  await axios.post(`http://localhost:${darkpoolPort}/sendOrder`, order)
     .then(function (response) {
       console.log(`Successfully submitted ${side} order for asset $${asset} @ ${limitPrice}`);
       token = response.data.orderNumber;
